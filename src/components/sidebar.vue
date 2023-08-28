b857c8365b323d9cdafdeca8b26d2dbc86b33e1d
Hisekawa <tetsuyakumiko01@gmail.com>
2023-08-28 14:33:17 +0700

    PT.RIMBA

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -67,7 +67,7 @@ export default {
       <ul class="space-y-2 font-medium">
         <li>
           <RouterLink
-            to="home"
+            to="/"
             class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
             <svg
               xmlns="http://www.w3.org/2000/svg"
