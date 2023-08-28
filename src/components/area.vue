e9292f5086f07dd5466e392fb5ed184a2c94009b
Hisekawa <tetsuyakumiko01@gmail.com>
2023-08-28 14:16:47 +0700

    PT.RIMBA

--- src/components/area.vue
+++ src/components/area.vue
@@ -94,7 +94,7 @@ let kenaikanuang = ((latestData - initialData) / initialData) * 100;
       <p class="text-base font-normal text-gray-500 dark:text-gray-400">Pendapatan</p>
     </div>
     <div
-  class="flex items-center px-2.5 py-0.5 text-base font-semibold text-green-500 dark:text-green-500 text-center">
+  class="flex items-center  py-0.5 text-base font-semibold text-green-500 dark:text-green-500 text-center">
   {{ kenaikanuang.toFixed(2) }}%
   <svg class="w-3 h-3 ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 14">
     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13V1m0 0L1 5m4-4 4 4"/>
--- src/components/user.vue
+++ src/components/user.vue
@@ -1,49 +1,99 @@
+<template>
+  <div class="relative overflow-x-auto">
+      <table class="w-full text-sm text-left text-black " id="myTable">
+          <thead class="text-xs text-black uppercase bg-gray-50 ">
+              <tr>
+                  <th scope="col" class="px-6 py-3">Name</th>
+                  <th scope="col" class="px-6 py-3">Gender</th>
+                  <th scope="col" class="px-6 py-3">Address</th>
+                  <th scope="col" class="px-6 py-3">Email</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr v-for="user in paginatedUsers" :key="user.id" class="bg-white border-b ">
+                  <td class="px-6 py-4 text-black">{{ user.firstName }} {{ user.lastName }}</td>
+                  <td class="px-6 py-4 text-black">{{ user.gender }}</td>
+                  <td class="px-6 py-4 text-black">{{ user.address.address }}</td>
+                  <td class="px-6 py-4 text-black">{{ user.email }}</td>
+              </tr>
+          </tbody>
+      </table>
+
+      <div class="mt-4 flex justify-between items-center"> 
+  <button  @click="previousPage" class="bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded-l">
+    Previous
+  </button>
+
+  <div class="flex">
+    <button 
+      v-for="page in pages" 
+      :key="page" 
+      @click="goToPage(page)" 
+      :class="{
+        'bg-indigo-600 text-black': currentPage === page,
+        'hover:bg-indigo-500': currentPage !== page,
+        'border': true,
+        'border-gray-400': true,
+        'px-3': true,
+        'py-1': true,
+        'mx-1': true
+      }"
+    >
+      {{ page }}
+    </button>
+  </div>
+
+  <button  @click="nextPage" class="bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded-r">
+    Next
+  </button>
+</div>
+  </div>
+</template>
+
 <script>
-import { ref, onMounted } from "vue";
-
 export default {
-  setup() {
-    const users = ref([]);
-
-    onMounted(async () => {
+  data() {
+    return {
+      users: [],
+      currentPage: 1,
+      itemsPerPage: 10
+    };
+  },
+  computed: {
+    paginatedUsers() {
+      const start = (this.currentPage - 1) * this.itemsPerPage;
+      const end = this.currentPage * this.itemsPerPage;
+      return this.users.slice(start, end);
+    },
+    totalPages() {
+      return Math.ceil(this.users.length / this.itemsPerPage);
+    },
+    pages() {
+      return Array.from({ length: this.totalPages }, (_, i) => i + 1);
+    }
+  },
+  methods: {
+    async fetchUsers() {
       const response = await fetch("https://dummyjson.com/users");
       const data = await response.json();
-      users.value = data.users;
-
-      setTimeout(() => {
-    new DataTable('#myTable', {
-      pagingType: 'full_numbers',
-      pageLength: 10  // Untuk menampilkan maksimal 10 entri per halaman
-    });
-  }, 0);
-    });
-
-    return {
-      users
-    };
+      this.users = data.users;
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+    previousPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+    goToPage(page) {
+      this.currentPage = page;
+    }
+  },
+  created() {
+    this.fetchUsers();
   }
 };
 </script>
-
-<template>
-    <div class="relative overflow-x-auto">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400" id="myTable">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                    <th scope="col" class="px-6 py-3">Name</th>
-                    <th scope="col" class="px-6 py-3">Gender</th>
-                    <th scope="col" class="px-6 py-3">Address</th>
-                    <th scope="col" class="px-6 py-3">Email</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="user in users" :key="user.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <td class="px-6 py-4">{{ user.firstName }} {{ user.lastName }}</td>
-                    <td class="px-6 py-4">{{ user.gender }}</td>
-                    <td class="px-6 py-4">{{ user.address.address }}</td>
-                    <td class="px-6 py-4">{{ user.email }}</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-</template>
